88494a3edbc1f94ea37e1c5edd42edf058ee87d2
Elerqsousy <mahmoud.eleresousy@gmail.com>
2023-04-10 23:39:36 +0200

    Fix routes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           <Routes>
             <Route
               index
+              path='/'
               element={
                 <React.Suspense fallback={<>...</>}>
                   <Home />
